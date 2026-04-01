3fb3dab7122df1bc8f9afde19e13a066eefafaec
etienne0114 <etiennetuyihamye@gmail.com>
2026-04-01 12:43:42 +0200

    chore: initial repository setup and configuration

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/server.ts',
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true,
+  testTimeout: 180000, // 3 minutes for integration tests
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
+};
